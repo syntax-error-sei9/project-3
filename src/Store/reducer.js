be4b35a48add6e18cd3202de34c1e66298fd3f86
gneral20 <gneral20@gmail.com>
2020-01-12 17:39:22 +0300

    fix conflict in App.js, reducer.js, Sidebar.jsx

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -12,9 +12,19 @@ const rootReducer = (state=inialState, action) => {
                topCities:action.value
            }
         case "SET_CITIES":
-            return{...state,cities: action.value}
+            return{
+                ...state,
+                cities: action.value
+            }
+        
+        case "Add_intrest":
+        return {
+            ...state,
+            intrests: action.value
+        }
         default:
             return state;
+   
     }
 }
 export default rootReducer;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import axios from 'axios';
 
 class SideBar extends Component {
-    state = {  }
     render() { 
-        return (  );
+        return ( 
+            <div>
+                {this.props.intrests.map(element => {
+                    return (
+                    <h1 key={element.intrestId}>{element.intrestName}</h1>
+                    )
+                })}
+            </div>
+         );
     }
+
+    componentDidMount(){
+        // const apiKey="&account=M48YWFOZ&token=8tobp16qxx6luhn0k0fhlou5m4h52poe"
+        // const apiHttps="https://www.triposo.com/api/20190906/"
+        // const citiesInfo = "location.json?&count=50" // need id of city , has array of images , has intro
+        // const apiUrl= apiHttps+citiesInfo+apiKey
+        let intrests=[]
+        axios.get('https://www.triposo.com/api/20190906/common_tag_labels.json')
+        .then(res => {
+          const results = res.data.results;
+          results.map(intrest => {
+            intrests.push({
+                intrestName: intrest.name,
+                intrestId: intrest.id,
+                intrestDesc: intrest.description
+             })
+    
+          })
+          this.props.setIntrest(intrests)
+        })
+    }
+
 }
- 
-export default SideBar;
+
+
+const getState = (state) => {
+  return {
+      intrests: state.intrests
+  }
+}
+
+const setState = (dispatch) => {
+    return {
+        setIntrest: (intrests) => dispatch({
+            type: "Add_intrest",
+            value: intrests
+        })
+    }
+  }
+
+export default connect(getState, setState) (SideBar);
