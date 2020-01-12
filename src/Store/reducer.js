f8b34f2e2f15d04e9eb81214db130aa42e245f56
monano <mona.gaihab@gmail.com>
2020-01-12 17:23:36 +0300

    add reducer.js

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -1,28 +1,22 @@
-const inialState = {
+const inialState ={
     cities: [],
     topCities: [],
     intrests: [],
     countries: [],
 }
-
-const rootReducer = (state = inialState, action) => {
+const rootReducer = (state=inialState, action) => {
     switch (action.type) {
-
-
-
-
-
-
-
         case "SET_TOP_CITIES":
-            return {
-                ...state,
-                topCities: action.value
-            }
-
-            default:
-                return state
+           return{
+               ...state,
+               topCities:action.value
+           }
+        default:
+            return state
+        case "SET_CITIES":
+            return{...state,cities: action.value}
+        default:
+            return state;
     }
 }
-
 export default rootReducer;
