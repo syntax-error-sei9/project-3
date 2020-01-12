93cef1bd927e88f0837da0dfdea86e4d6343b9cd
gneral20 <gneral20@gmail.com>
2020-01-12 11:17:27 +0300

    Redux State managment has been instaled and configerd

---
+++ src/Store/reducer.js
@@ -0,0 +1,12 @@
+const inialState ={
+    cities: [],
+    topCities: [],
+    intrests: [],
+    countries: [],
+}
+
+const rootReducer = (state=inialState, action) => {
+    return state
+}
+
+export default rootReducer;
