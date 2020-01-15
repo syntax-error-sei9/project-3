2ce908abe590373cb12144695d9e9c04703182eb
gneral20 <gneral20@gmail.com>
2020-01-15 16:48:53 +0300

    Updates

---
+++ precache-manifest.ac5045d7a9d71d8a4917abc8885a8b9d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2735ba2dc314ea38a764e0f1a3118bed",
+    "url": "/project-3/index.html"
+  },
+  {
+    "revision": "69e384ad104614dc742b",
+    "url": "/project-3/static/css/2.4fec2d97.chunk.css"
+  },
+  {
+    "revision": "6e460fe423988c3bf238",
+    "url": "/project-3/static/css/main.bc0064a2.chunk.css"
+  },
+  {
+    "revision": "69e384ad104614dc742b",
+    "url": "/project-3/static/js/2.1f778609.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/project-3/static/js/2.1f778609.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e460fe423988c3bf238",
+    "url": "/project-3/static/js/main.2d5383e1.chunk.js"
+  },
+  {
+    "revision": "c639e20371c45bdc0825",
+    "url": "/project-3/static/js/runtime-main.8dddfbf0.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/project-3/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/project-3/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/project-3/static/media/revicons.2feb69cc.eot"
+  }
+]);
